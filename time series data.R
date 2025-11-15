3a09e77ba72b077297c34f118eab87ffeb11ab2b
abhishekj529 <mudhirajabhishek143@gmail.com>
2025-11-15 16:50:04 +0530

    Add files via upload

---
+++ time series data.R
@@ -0,0 +1,18 @@
+# Getting the data points in form of a R vector.   
+snowfall <- c(790,1170.8,860.1,1330.6,630.4,911.5,683.5,996.6,783.2,982,881.8,1021)   
+# Convertting it into a time series object.   
+snowfall_timeseries<- ts(snowfall,start = c(2013,1),frequency = 12) 
+png(file = "snowfall.png") 
+# Printing the timeseries data.   
+print(snowfall_timeseries)   
+# Giving a name to the chart file.   
+  
+# Plotting a graph of the time series.   
+plot(snowfall_timeseries)   
+# Saving the file.   
+model <- lm(snowfall_timeseries ~ time(snowfall_timeseries))
+plot(snowfall_timeseries, main="Snowfall Trend", xlab="Time", ylab="Snowfall", col="blue")
+abline(model, col="red")
+
+dev.off()  
+
