62b52fade1cfb88fec76b0f3a49a709b05801aac
abhishekj529 <mudhirajabhishek143@gmail.com>
2025-11-15 16:56:09 +0530

    Add files via upload

---
+++ pnorm.R
@@ -0,0 +1,48 @@
+# Create a sequence of numbers between -10 and 10 incrementing by 0.2. 
+x <- seq(-10,10,by = .2) 
+
+# Choose the mean as 2.5 and standard deviation as 2.  
+y <- pnorm(x, mean = 2.5, sd = 2) #cumulative distribution function
+
+# Give the chart file a name. 
+png(file = "pnorm.png") 
+
+# Plot the graph. 
+plot(x,y) 
+
+# Save the file. 
+dev.off() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
